311209cc83875b8e43dc15e37958f94f75b1e4b9
Edith Puclla <edypuclla@gmail.com>
2023-08-08 16:03:15 -0500

    Fix path of script

--- python-app/scripts/setup.sh
+++ python-app/scripts/setup.sh
@@ -16,12 +16,12 @@ psql -U postgres -h localhost -d postgres -c "SELECT * FROM citus_version();"
 
 echo "$SEPARATOR Creating Database $SEPARATOR"
 #psql -U postgres -h localhost -c "CREATE DATABASE startup;"
-psql -U postgres -h localhost -f data.sql
+psql -U postgres -h localhost -f ../sql/data.sql
 
 echo "$SEPARATOR Copy database files into Docker container $SEPARATOR"
-sudo docker cp companies.csv citus_coordinator:.
-sudo docker cp campaigns.csv citus_coordinator:.
-sudo docker cp ads.csv citus_coordinator:.
+sudo docker cp ../csv/companies.csv citus_coordinator:.
+sudo docker cp ../csv/campaigns.csv citus_coordinator:.
+sudo docker cp ../csv/ads.csv citus_coordinator:.
 
 echo "$SEPARATOR Populating data into companies, campaigns and ads tables $SEPARATOR"
 psql -U postgres -h localhost -d postgres -c "\copy companies from 'companies.csv' with csv;"
